37600faba9d4b113d22562e80eed38e09eabb01f
Oleg Ovcharuk <vgvoleg@gmail.com>
2024-10-29 15:10:11 +0300

    remove parameters from YdbQuery

--- ydb_dbapi/cursors.py
+++ ydb_dbapi/cursors.py
@@ -1,8 +1,60 @@
-class Cursor:
-    def __init__(self):
-        self.arraysize = 1
+import dataclasses
+from typing import (
+    Any,
+    Dict,
+    List,
+    Optional,
+    Tuple,
+    Union,
+)
 
-    async def execute(self):
+import ydb
+
+
+ParametersType = Dict[
+    str,
+    Union[
+        Any,
+        Tuple[Any, Union[ydb.PrimitiveType, ydb.AbstractTypeBuilder]],
+        ydb.TypedValue,
+    ],
+]
+
+
+@dataclasses.dataclass
+class YdbQuery:
+    yql_text: str
+    is_ddl: bool = False
+
+
+class Cursor:
+    def __init__(
+        self,
+        session_pool: ydb.aio.QuerySessionPool,
+        session: ydb.aio.QuerySession,
+        tx_mode: ydb.BaseQueryTxMode,
+        tx_context: Optional[ydb.QueryTxContext] = None,
+        table_path_prefix: str = "",
+    ):
+        self.arraysize = 1
+        self._description = None
+
+        self._pool = session_pool
+        self._session = session
+        self._tx_mode = tx_mode
+        self._tx_context = tx_context
+        self._table_path_prefix = table_path_prefix
+
+    async def _execute_ddl_query(
+        self, query: YdbQuery, parameters: Optional[ParametersType] = None
+    ) -> List[ydb.convert.ResultSet]:
+        return await self._pool.execute_with_retries(
+            query=query, parameters=parameters
+        )
+
+    async def execute(
+        self, operation: YdbQuery, parameters: Optional[ParametersType] = None
+    ):
         pass
 
     async def executemany(self):
