7bc3bb20e7a8d80bb9095fe5431316786e1f4830
Oleg Ovcharuk <vgvoleg@gmail.com>
2024-10-29 15:11:04 +0300

    publish action

--- ydb_dbapi/__init__.py
+++ ydb_dbapi/__init__.py
@@ -3,6 +3,7 @@ from .connections import Connection
 from .connections import IsolationLevel
 from .connections import async_connect
 from .connections import connect
+from .constants import *
 from .cursors import AsyncCursor
 from .cursors import Cursor
 from .errors import *
