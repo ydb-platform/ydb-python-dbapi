0aa46006ca44a956428a814f37800a6d033b094e
Oleg Ovcharuk <vgvoleg@gmail.com>
2026-08-27 14:30:47 +0300

    Release session in close() even if rollback fails (#43)
    * Release session in close() even if rollback fails
    
    * Restore patched methods in close() regression test

--- tests/test_connections.py
+++ tests/test_connections.py
@@ -66,6 +66,43 @@ class BaseDBApiTestSuit:
             maybe_await(connection.rollback())
 
 
+    def _test_close_releases_session_after_rollback_error(
+        self,
+        connection: dbapi.Connection,
+    ) -> None:
+        connection.set_isolation_level(dbapi.IsolationLevel.SERIALIZABLE)
+        maybe_await(connection.begin())
+
+        acquired_session = connection._session
+        assert acquired_session is not None
+
+        released = []
+        session_pool = connection._session_pool
+        tx_context = connection._tx_context
+        original_release = session_pool.release
+        original_rollback = tx_context.rollback
+
+        def release(session: ydb.QuerySession) -> any:
+            released.append(session)
+            return original_release(session)
+
+        def failing_rollback(*args: any, **kwargs: any) -> None:
+            raise ydb.issues.BadSession("session is invalidated")
+
+        session_pool.release = release
+        tx_context.rollback = failing_rollback
+
+        try:
+            with pytest.raises(dbapi.Error):
+                maybe_await(connection.close())
+
+            assert released == [acquired_session]
+            assert connection._session is None
+            assert connection._tx_context is None
+        finally:
+            session_pool.release = original_release
+            tx_context.rollback = original_rollback
+
     def _test_connection(self, connection: dbapi.Connection) -> None:
         maybe_await(connection.commit())
         maybe_await(connection.rollback())
@@ -466,6 +503,11 @@ class TestConnection(BaseDBApiTestSuit):
             connection, isolation_level
         )
 
+    def test_close_releases_session_after_rollback_error(
+        self, connection: dbapi.Connection
+    ) -> None:
+        self._test_close_releases_session_after_rollback_error(connection)
+
     def test_connection(self, connection: dbapi.Connection) -> None:
         self._test_connection(connection)
 
@@ -578,6 +620,14 @@ class TestAsyncConnection(BaseDBApiTestSuit):
             self._test_commit_rollback_after_begin,
             connection,
             isolation_level
+        )
+
+    @pytest.mark.asyncio
+    async def test_close_releases_session_after_rollback_error(
+        self, connection: dbapi.AsyncConnection
+    ) -> None:
+        await greenlet_spawn(
+            self._test_close_releases_session_after_rollback_error, connection
         )
 
     @pytest.mark.asyncio
